7d6ebe51a4c3825f31b3aa4977678126e6c6ef33
Joel <jvbrinkley21@gmail.com>
2018-03-26 14:52:02 -0500

    removed azure-build script

--- azure-build.ps1
+++
@@ -1,5 +0,0 @@
-docker build -f .\docker\TodoService\dockerfile -t tstraubk8shackfest.azurecr.io/todo-service:v1 .
-docker push tstraubk8shackfest.azurecr.io/todo-service:v1
-
-docker build -f .\docker\TodoWebApp\dockerfile -t tstraubk8shackfest.azurecr.io/todo-webapp:v1 .
-docker push tstraubk8shackfest.azurecr.io/todo-webapp:v1
