35adc3d6d4f8c640797f3f3b38673469ccf6690c
Joel <jbrinkley@cardinalsolutions.com>
2018-11-28 16:30:59 -0600

    configurable k8s

--- terraform/application/provider.tf
+++ terraform/application/provider.tf
@@ -1,2 +1,12 @@
 
-provider "kubernetes" {}
+provider "kubernetes" {
+    host                   = "${var.host}"
+    client_certificate     = "${base64decode(var.client_certificate)}"
+    client_key             = "${base64decode(var.client_key)}"
+    cluster_ca_certificate = "${base64decode(var.cluster_ca_certificate)}"
+    load_config_file = false
+    insecure               = "${var.insecure}"
+}
+
+
+
